505b0fa50d10148df1500164bbd23c0d2b8a7ee3
Arthur Kennedy Otieno <TheBeachMaster@users.noreply.github.com>
2019-04-13 17:10:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var assign = require('deep-assign');
 var renderer = require('./lib/renderer');
 
 hexo.config.diagrams = assign({
-  mermaid: 'https://cdn.bootcss.com/mermaid/7.1.2/mermaid.js'
+  mermaid: 'https://unpkg.com/mermaid@8.0.0/dist/mermaid.js'
 }, hexo.config.diagrams);
 
 hexo.extend.filter.register('before_post_render', renderer.render, 9);
